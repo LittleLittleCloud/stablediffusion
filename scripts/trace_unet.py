89912d03fdd9f4541a7cfd7c6a55243751ce8b1f
littlelittlecloud <g220578356@gmail.com>
2023-04-07 22:32:21 +0000

    update

--- scripts/trace_unet.py
+++ scripts/trace_unet.py
@@ -5,36 +5,68 @@ from omegaconf import OmegaConf
 from pytorch_lightning import seed_everything
 import torch
 import torch.nn as nn
+from ldm.modules.distributions.distributions import DiagonalGaussianDistribution
 from ldm.util import load_model_from_config
 from ldm.modules.diffusionmodules.util import extract_into_tensor
 import torchvision
 from torch import autocast
 import os
 import numpy as np
+import open_clip
 
-class DDIMSampler(nn.Module):
-    def __init__(self, model, scale):
+class DDPM(nn.Module):
+    def __init__(self, model):
         super().__init__()
         self.model = model
-        self.scale = scale
 
-    @autocast('cuda')
     @torch.no_grad()
-    def forward(self, img, condition, unconditional_condition, t, t_prev):
+    def diffusion_model_1(self, img, condition, unconditional_condition, t, t_prev):
         x_in = torch.cat([img] * 2)
         t_in = torch.cat([t] * 2)
         condition_in = torch.cat([unconditional_condition, condition])
         e_t_uncond, e_t = self.model.model.diffusion_model(x_in, t_in, condition_in).chunk(2)
         model_output = e_t_uncond + self.scale * (e_t - e_t_uncond)
         e_t = self.model.predict_eps_from_z_and_v(img, t, model_output)
-            # alpha_bar_t = torch.full((opt.batch, 1, 1, 1), alpha_bars[index], dtype=torch.float16, device=device)
-            # alpha_bar_t_prev = torch.full((opt.batch, 1, 1, 1), alpha_bars_prev[index], dtype=torch.float16, device=device)
-            # pred_x0 = (img - torch.sqrt(1-alpha_bar_t) * e_t) / torch.sqrt(alpha_bar_t)
-        # pred_x0 = extract_into_tensor(sqrt_alphas_bar, t, img.shape) * img - extract_into_tensor(sqrt_one_minus_alphas_bar, t, img.shape) * model_output
         pred_x0 = self.model.predict_start_from_z_and_v(img, t, model_output)
-        img = self.model.q_sample(pred_x0, t_prev, e_t)
+        img = self.model.predict_start_from_z_and_v(pred_x0, t_prev, e_t)
         return img
+    
+    @torch.no_grad()
+    def diffusion_model(self, x_in, t_in, condition_in):
+        print(self.model.model.diffusion_model.use_checkpoint)
+        return self.model.model.diffusion_model(x_in, t_in, condition_in)
 
+    @torch.no_grad()
+    def predict_eps_from_z_and_v(self, z, t, v):
+        return self.model.predict_eps_from_z_and_v(z, t, v)
+    
+    @torch.no_grad()
+    def predict_start_from_z_and_v(self, z, t, v):
+        return self.model.predict_start_from_z_and_v(z, t, v)
+    
+    @torch.no_grad()
+    def q_sample(self, z, t, v):
+        return self.model.q_sample(z, t, v)
+    
+
+    @torch.no_grad()
+    def decode_image(self, img):
+        return self.model.decode_first_stage(img)
+    
+    @torch.no_grad()
+    def clip_encoder(self, tokens):
+        encode = self.model.cond_stage_model.encode_with_transformer(tokens)
+        return encode
+
+class DDPMDecoder(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    @autocast('cuda')
+    @torch.no_grad()
+    def forward(self, img):
+        return self.model.decode_first_stage(img)
 
 def main(opt):
     torch.set_grad_enabled(False)
@@ -42,68 +74,64 @@ def main(opt):
     config = OmegaConf.load(f"{opt.config}")
     device = torch.device("cuda") if opt.device == "cuda" else torch.device("cpu")
     model = load_model_from_config(config, f"{opt.ckpt}", device=device, verbose=True)
+    print(model.model.diffusion_model.use_checkpoint)
     scale = 9.0
-    sampler = DDIMSampler(model, 9.0)
-    with torch.no_grad():
+    sampler = DDPM(model)
+    with torch.no_grad(), autocast('cuda'):
         img_in = torch.ones(opt.batch, 4, 96, 96, dtype=torch.float32, device = device)
         t_in = torch.ones(opt.batch, dtype=torch.int64, device = device)
         t_prev = torch.ones(opt.batch, dtype=torch.int64, device = device)
         context = torch.ones(opt.batch, 77, 1024, dtype=torch.float32, device = device)
+        tokens = torch.ones((opt.batch, 77), dtype=torch.int64, device = device)
         uncondition_context = torch.ones(opt.batch, 77, 1024, dtype=torch.float32, device = device)
-        scripted_sampler = torch.jit.trace(sampler, (img_in, context, uncondition_context, t_in, t_prev))
-        # samples_ddim = torch.randn(opt.batch, 4, 96, 96, device=device)
-        # input = {
-        #     'forward': (img_in, t_in, context),
-        #     # 'decode_first_stage': (samples_ddim),
-        # }
-        # scripted_diffusion_model = torch.jit.trace(model.model.diffusion_model, (img_in, t_in, context))
-        # # scripted_unet.save(f"{opt.output}/unet.pt")
+        input = {
+            'diffusion_model': (torch.cat([img_in]),torch.cat([t_in]), torch.cat([context])),
+            # 'decode_image': (img_in, ),
+            # 'clip_encoder': (tokens, ),
+            # 'predict_eps_from_z_and_v': (img_in, t_in, img_in),
+            # 'predict_start_from_z_and_v': (img_in, t_in, img_in),
+            # 'q_sample': (img_in, t_in, img_in),
+        }
+        # scripted_sampler = sampler
+        scripted_sampler = torch.jit.trace_module(sampler, input)
+        # scripted_sampler.save(f"{opt.output}/ddim_v_sampler.pt")
 
         # # get Decoder for first stage model scripted
-        # scripted_decoder = torch.jit.trace_module(model, {'decode_first_stage': samples_ddim})
-        # # scripted_decoder.save(f"{opt.output}/decoder.pt")
+        # samples_ddim = torch.randn(opt.batch, 4, 96, 96, device=device)
+        # scripted_decoder = torch.jit.trace(decoder, (samples_ddim))
+        # scripted_decoder.save(f"{opt.output}/decoder.pt")
 
         n_step = 50
         timesteps = 1000
         ddim_timesteps = np.asarray(list(range(0, timesteps, timesteps // n_step)))
         ddim_timesteps = ddim_timesteps + 1
-
-        betas = torch.linspace(0.00085, 0.012, timesteps, device=device)
-        alphas = 1.0 - betas
-        alpha_bars = torch.cumprod(alphas, 0)
         # betas = model.betas
         # alpha_bars = model.alphas_cumprod
-        sqrt_alphas_bar = torch.sqrt(alpha_bars)
-        sqrt_one_minus_alphas_bar = torch.sqrt(1.0 - alpha_bars)
-
-        alpha_bars_prev = torch.cat((torch.full((1,),alpha_bars[0], device=device), alpha_bars[ddim_timesteps[:-1]] ), 0)
-        alpha_bars = alpha_bars[ddim_timesteps]
         img = torch.randn(opt.batch, 4, 96, 96, device=device)
         t = torch.ones(opt.batch, dtype=torch.int32, device=device)
-        prompts = opt.batch * ["cute black cat"]
-        condition = model.get_learned_conditioning(prompts)
-        unconditional_condition = model.get_learned_conditioning(opt.batch * [""])
+        prompts = opt.batch * ["cute green dog"]
+        tokens = open_clip.tokenize(prompts)
+        tokens = tokens.to(device)
+        condition = scripted_sampler.clip_encoder(tokens)
+        unconditional_prompt = opt.batch * [""]
+        unconditional_tokens = open_clip.tokenize(unconditional_prompt)
+        unconditional_tokens = unconditional_tokens.to(device)
+        unconditional_condition = scripted_sampler.clip_encoder(unconditional_tokens)
         for index, i in enumerate(reversed(ddim_timesteps)):
             index = n_step - index - 1
             t = torch.full((opt.batch,), i, dtype=torch.int64, device=device)
             t_prev = torch.full((opt.batch,), i - 20 if i - 20 >= 0 else 0, dtype=torch.int64, device=device)
-            img = scripted_sampler(img, condition, unconditional_condition, t, t_prev)
-            # x_in = torch.cat([img] * 2)
-            # t_in = torch.cat([t] * 2)
-            # condition_in = torch.cat([unconditional_condition, condition])
-            # e_t_uncond, e_t = scripted_diffusion_model(x_in, t_in, condition_in).chunk(2)
-            # model_output = e_t_uncond + scale * (e_t - e_t_uncond)
-            # e_t = extract_into_tensor(sqrt_alphas_bar, t, img.shape) * model_output + extract_into_tensor(sqrt_one_minus_alphas_bar, t, img.shape) * img
-            # # e_t = model.predict_eps_from_z_and_v(img, t, model_output)
-            # # alpha_bar_t = torch.full((opt.batch, 1, 1, 1), alpha_bars[index], dtype=torch.float16, device=device)
-            # alpha_bar_t_prev = torch.full((opt.batch, 1, 1, 1), alpha_bars_prev[index], dtype=torch.float16, device=device)
-            # # pred_x0 = (img - torch.sqrt(1-alpha_bar_t) * e_t) / torch.sqrt(alpha_bar_t)
-            # pred_x0 = extract_into_tensor(sqrt_alphas_bar, t, img.shape) * img - extract_into_tensor(sqrt_one_minus_alphas_bar, t, img.shape) * model_output
-            # # pred_x0 = model.predict_start_from_z_and_v(img, t, model_output)
-            # img = torch.sqrt(alpha_bar_t_prev) * pred_x0 + torch.sqrt(1-alpha_bar_t_prev) * e_t
+            x_in = torch.cat([img]*2)
+            t_in = torch.cat([t]*2)
+            condition_in = torch.cat([unconditional_condition, condition])
+            e_t_uncond, e_t = scripted_sampler.diffusion_model(x_in, t_in, condition_in).chunk(2)
+            model_output = e_t_uncond + 9.0 * (e_t - e_t_uncond)
+            e_t = scripted_sampler.predict_eps_from_z_and_v(img, t, model_output)
+            pred_x0 = scripted_sampler.predict_start_from_z_and_v(img, t, model_output)
+            img = scripted_sampler.q_sample(pred_x0, t_prev, e_t)
+            print(img.dtype)
 
-        # img = 1. / model.scale_factor * img
-        decoded_images = sampler.model.decode_first_stage(img)
+        decoded_images = scripted_sampler.decode_image(img)
         decoded_images = torch.clamp((decoded_images + 1.0) / 2.0, 0.0, 1.0)
         outpath = opt.output
         base_count=0
